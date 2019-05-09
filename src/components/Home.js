fe5c6f3833b140e8cd8b312483eab4989026f48a
Michael Pettit <18519961@students.ltu.edu.au>
2019-05-09 23:07:21 +1000

    Added comments

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,8 +4,16 @@ import Map from './Map';
 import { Provider } from 'react-redux';
 import { store } from '../redux/store'
 
+// Create a react component called "Home"
 class Home extends React.Component {
+
+    // Render function is what the component looks like
     render() {
+
+        // Returns JSX (HTML)
+        // <Provider store={store}> gives the components inside it access to the react store
+        // <Map/> is the Map.js component
+        // <NodeList/> is the NodeList.js component
         return (
             <Provider store={store}>
                 <div className='map'>
@@ -17,4 +25,5 @@ class Home extends React.Component {
     }
 }
 
+// Exports the "Home" object for use in other files
 export default Home;
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,63 +1,31 @@
 import React from 'react';
 import mapboxgl from 'mapbox-gl'
+
+// Public key for mapbox API
 mapboxgl.accessToken = 'pk.eyJ1IjoiMTg1MTk5NjEiLCJhIjoiY2p2OWd4bThtMHNwNDN5cDU0OWZ6aTczeiJ9.I0UeX3pGMBHSet68Nx9R4w';
 
+// Options list for toggle (toggle just for testing)
 const options = [{
-  name: 'Population',
-  description: 'Estimated total population',
-  property: 'pop_est',
-  stops: [
-    [0, '#f8d5cc'],
-    [1000000, '#f4bfb6'],
-    [5000000, '#f1a8a5'],
-    [10000000, '#ee8f9a'],
-    [50000000, '#ec739b'],
-    [100000000, '#dd5ca8'],
-    [250000000, '#c44cc0'],
-    [500000000, '#9f43d7'],
-    [1000000000, '#6e40e6']
-  ]
-}, {
-  name: 'GDP',
-  description: 'Estimate total GDP in millions of dollars',
-  property: 'gdp_md_est',
-  stops: [
-    [0, '#f8d5cc'],
-    [1000, '#f4bfb6'],
-    [5000, '#f1a8a5'],
-    [10000, '#ee8f9a'],
-    [50000, '#ec739b'],
-    [100000, '#dd5ca8'],
-    [250000, '#c44cc0'],
-    [5000000, '#9f43d7'],
-    [10000000, '#6e40e6']
-  ]
-},
-{
-  name: 'Test',
-  description: 'Estimated total population',
-  property: 'pop_est',
-  stops: [
-    [0, '#f8d5cc'],
-    [1000000, '#f4bfb6'],
-    [5000000, '#f1a8a5'],
-    [10000000, '#ee8f9a'],
-    [50000000, '#ec739b'],
-    [100000000, '#dd5ca8'],
-    [250000000, '#c44cc0'],
-    [500000000, '#9f43d7'],
-    [1000000000, '#6e40e6']
-  ]
-}]
+    name: 'Population',
+  }, {
+    name: 'GDP',
+  }, {
+    name: 'Test',
+  }
+]
 
-// Define popup
-  var popup = new mapboxgl.Popup({anchor: 'top-left', className: 'my-class'})
+// Defines a popup
+var popup = new mapboxgl.Popup({anchor: 'top-left', className: 'my-class'})
   .setHTML("<h1>Hello World!</h1>")
   .setMaxWidth("300px")
 
+// Defines Map component
 class Map extends React.Component {
 
+  // If/when the component mounts
   componentDidMount() {
+
+    //create a new map object, centers it and sets default zoom
     this.map = new mapboxgl.Map({
       container: this.mapContainer,
       style: 'mapbox://styles/mapbox/streets-v9',
@@ -65,6 +33,7 @@ class Map extends React.Component {
       zoom: 11
     });
 
+    // Add a marker to the map
     const addMarker = () => {
         new mapboxgl.Marker()
           .setLngLat([144.278,-36.759])
