448c4dcd72f4f5c34a094e07d4343b0779f67158
Michael Pettit <subnormalnebula@gmail.com>
2019-05-21 20:51:06 +1000

    added more API endpoints

--- api/data/settings.js
+++ api/data/settings.js
@@ -120,14 +120,14 @@ module.exports = {
     // -----------------
     // To password protect the Node-RED editor and admin API, the following
     // property can be used. See http://nodered.org/docs/security.html for details.
-    adminAuth: {
-        type: "credentials",
-        users: [{
-            username: "admin",
-            password: "$2b$08$Bg0umfwHq4cAHUSknGDgkOglFudHrdjBKcQcL4MImXKxtghbcB2m2",
-            permissions: "*"
-        }]
-    },
+    //adminAuth: {
+    //    type: "credentials",
+    //    users: [{
+    //        username: "admin",
+    //        password: "$2b$08$Bg0umfwHq4cAHUSknGDgkOglFudHrdjBKcQcL4MImXKxtghbcB2m2",
+    //        permissions: "*"
+    //    }]
+    //},
 
     // To password protect the node-defined HTTP endpoints (httpNodeRoot), or
     // the static content (httpStatic), the following properties can be used.
