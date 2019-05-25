e3db43e7138a0e64bf011032205b80045772d802
Michael Pettit <18519961@students.ltu.edu.au>
2019-05-25 15:00:22 +1000

    Added code for no database connection

--- src/components/InfoTableRow.js
+++ src/components/InfoTableRow.js
@@ -5,18 +5,22 @@ class InfoTableRow extends React.Component {
     render() {
 
         const convertTime = (time) => {
-            return new Date(time).toLocaleTimeString();
+            var date = new Date(time).toLocaleTimeString('default', {hour: '2-digit', minute: '2-digit'});
+            if (date !== "Invalid Date"){
+                return date;
+            }
         }
 
         return (
             <tr className="node">
-                <td>{convertTime(this.props.row.time)}</td>
+                <td>{convertTime(this.props.row.time) || this.props.row.time}</td>
                 <td>
                     {
                         this.props.row.pedl +
                         this.props.row.pedr +
                         this.props.row.cycll +
-                        this.props.row.cyclr
+                        this.props.row.cyclr ||
+                        this.props.row.count
                     }
                 </td>
                 <td>{this.props.row.temp + String.fromCharCode(176)}C</td>
--- src/redux/reducers/TableData.js
+++ src/redux/reducers/TableData.js
@@ -9,54 +9,54 @@ const initialState = {
         {   
             location:"View St", 
             data: [
-                {"Time":"08:20","Count":50,"Temp":32},
-                {"Time":"08:30","Count":51,"Temp":32},
-                {"Time":"08:40","Count":40,"Temp":31},
-                {"Time":"08:50","Count":32,"Temp":31},
-                {"Time":"09:00","Count":45,"Temp":32},
-                {"Time":"09:10","Count":43,"Temp":32},
-                {"Time":"09:20","Count":36,"Temp":33},
-                {"Time":"09:30","Count":18,"Temp":33}
+                {"time":"08:20","count":50,"temp":32},
+                {"time":"08:30","count":51,"temp":32},
+                {"time":"08:40","count":40,"temp":31},
+                {"time":"08:50","count":32,"temp":31},
+                {"time":"09:00","count":45,"temp":32},
+                {"time":"09:10","count":43,"temp":32},
+                {"time":"09:20","count":36,"temp":33},
+                {"time":"09:30","count":18,"temp":33}
             ]
         },
         {
             location:"Bernard St", 
             data: [
-                {"Time":"08:20","Count":12,"Temp":32},
-                {"Time":"08:30","Count":15,"Temp":32},
-                {"Time":"08:40","Count":10,"Temp":31},
-                {"Time":"08:50","Count":7,"Temp":31},
-                {"Time":"09:00","Count":19,"Temp":32},
-                {"Time":"09:10","Count":8,"Temp":32},
-                {"Time":"09:20","Count":16,"Temp":33},
-                {"Time":"09:30","Count":18,"Temp":33}
+                {"time":"08:20","count":12,"temp":32},
+                {"time":"08:30","count":15,"temp":32},
+                {"time":"08:40","count":10,"temp":31},
+                {"time":"08:50","count":7,"temp":31},
+                {"time":"09:00","count":19,"temp":32},
+                {"time":"09:10","count":8,"temp":32},
+                {"time":"09:20","count":16,"temp":33},
+                {"time":"09:30","count":18,"temp":33}
         
         ]
         },
         {   
             location:"Henry St", 
             data: [
-                {"Time":"08:20","Count":57,"Temp":32},
-                {"Time":"08:30","Count":58,"Temp":32},
-                {"Time":"08:40","Count":43,"Temp":31},
-                {"Time":"08:50","Count":34,"Temp":31},
-                {"Time":"09:00","Count":42,"Temp":32},
-                {"Time":"09:10","Count":44,"Temp":32},
-                {"Time":"09:20","Count":32,"Temp":33},
-                {"Time":"09:30","Count":11,"Temp":33}
+                {"time":"08:20","count":57,"temp":32},
+                {"time":"08:30","count":58,"temp":32},
+                {"time":"08:40","count":43,"temp":31},
+                {"time":"08:50","count":34,"temp":31},
+                {"time":"09:00","count":42,"temp":32},
+                {"time":"09:10","count":44,"temp":32},
+                {"time":"09:20","count":32,"temp":33},
+                {"time":"09:30","count":11,"temp":33}
             ]
         },
         {
             location:"Forest St", 
             data: [
-                {"Time":"08:20","Count":30,"Temp":32},
-                {"Time":"08:30","Count":21,"Temp":32},
-                {"Time":"08:40","Count":30,"Temp":31},
-                {"Time":"08:50","Count":42,"Temp":31},
-                {"Time":"09:00","Count":15,"Temp":32},
-                {"Time":"09:10","Count":13,"Temp":32},
-                {"Time":"09:20","Count":16,"Temp":33},
-                {"Time":"09:30","Count":28,"Temp":33}
+                {"time":"08:20","count":30,"temp":32},
+                {"time":"08:30","count":21,"temp":32},
+                {"time":"08:40","count":30,"temp":31},
+                {"time":"08:50","count":42,"temp":31},
+                {"time":"09:00","count":15,"temp":32},
+                {"time":"09:10","count":13,"temp":32},
+                {"time":"09:20","count":16,"temp":33},
+                {"time":"09:30","count":28,"temp":33}
             ]
         }
     ]
@@ -65,17 +65,24 @@ const initialState = {
 function reducer(state = initialState, action) {
     
     switch(action.type) {
-        case SHOW_TABLE: 
-            return Object.assign({}, state, {
-                visible: true,
-                location: action.location,
-                data: action.data
-                
-                    /*initialState.data.filter( data => {
-                    return data.location === action.location; 
-                    */
-                //})[0] // Have to get the 0th element of the returned array for some reason 
-            })
+        case SHOW_TABLE: {
+
+            if (action.data === null) {
+                return Object.assign({}, state, {
+                    visible: true,
+                    location: action.location,
+                    data: initialState.data.filter( data => {
+                            return data.location === action.location; 
+                    })[0].data // Have to get the 0th element of the returned array for some reason 
+                })
+            } else {
+                return Object.assign({}, state, {
+                    visible: true,
+                    location: action.location,
+                    data: action.data
+                })
+            }
+        }
         
         case HIDE_TABLE:
             return Object.assign({}, state, {
@@ -102,6 +109,11 @@ export function showTable(id, location) {
         })
         .catch(error => {
             console.log(error);
+            dispatch({
+                type: SHOW_TABLE, 
+                location: location, 
+                data: null
+            });
         });
     }
 }
