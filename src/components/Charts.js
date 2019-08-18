de6efa1e47b4572022eb52ab4752157c28149888
Charles <18590775@students.latrobe.edu.au>
2019-08-18 14:44:22 +0800

    Newest prototype of Line graph

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Chart from "react-apexcharts";
+import ReactApexChart from "react-apexcharts";
 
 class Charts extends Component {
   constructor(props) {
@@ -8,38 +8,82 @@ class Charts extends Component {
     this.state = {
       options: {
         chart: {
-          id: "count-bar"
+          shadow: {
+            enabled: true,
+            color: '#000',
+            top: 18,
+            left: 7,
+            blur: 10,
+            opacity: 1
+          },
+          toolbar: {
+            show: false
+          }
+        },
+        colors: ['#77B6EA', '#545454'],
+        dataLabels: {
+          enabled: true,
+        },
+        stroke: {
+          curve: 'smooth'
+        },
+
+        grid: {
+          borderColor: '#e7e7e7',
+          row: {
+            colors: ['#f3f3f3', 'transparent'], 
+            opacity: 0.5
+          },
+        },
+        markers: {
+          
+          size: 3
         },
         xaxis: {
-          categories: [9.00,10.00,11.00,12.00,13.00,14.00,15.00,16.00,17.00]
+          categories: ['9', '10', '11', '12', '13', '14', '15','16'],
+          title: {
+            text: 'Time'
+          }
+        },
+        yaxis: {
+          title: {
+            text: 'counter'
+          },
+          min: 5,
+          max: 40
+        },
+        legend: {
+          position: 'top',
+          horizontalAlign: 'right',
+          floating: true,
+          offsetY: -25,
+          offsetX: -5
         }
       },
       series: [
         {
-          name: "counts",
-          data: [30, 40, 45, 50, 49, 60, 70, 91]
+          name: "Walker",
+          data: [28, 29, 33, 36, 32, 32, 33]
+        },
+        {
+          name: "Vehicle",
+          data: [12, 11, 14, 18, 17, 13, 13]
         }
-      ]
-    };
+      ],
+    }
   }
 
   render() {
+
     return (
-      <div className="app">
-        <div className="row">
-          <div className="mixed-chart">
-            <Chart
-                options={this.state.options}
-               series={this.state.series}
-               type="line"
-               width="300"
-             
-            />
-          </div>
+      
+
+        <div id="chart">
+          <ReactApexChart options={this.state.options} series={this.state.series} type="line" height="250"width="250" />
         </div>
-      </div>
-    );
-  }
-}
+              );
+            }
+          }
+        
 
 export default Charts;
